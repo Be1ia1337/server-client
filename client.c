54deb43d7e412841a147f5043756cfa1c611baf7
Be1ia1337 <asimonyan.work@gmail.com>
2022-04-18 14:19:41 +0400

    Server and Client v1

---
+++ client.c
@@ -0,0 +1,268 @@
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define MESSAGE_LEN 80
+#define COMMAND_SIZE 40
+
+int listening_socket;
+int is_connected;
+char *p_command;
+
+/** @brief connection with server
+ * 	@param listening_socket is socket that will beconnected to ip and port
+ *  @param servaddr is structure for assigning ip and port of server
+ * 	@param ip we will connect
+ * 	@param port we will connect
+ * 	@return void
+ */
+int connection(int listening_socket, struct sockaddr_in* servaddr, char* ip, unsigned short int port);
+
+/** @brief sending, recievinf and working wih messages
+ * 	@param listening_socket is created socket that will help with communication
+ * 	@param command is message that will be send to server
+ * 	@return void
+ */
+void command_execution(int listening_socket, char* command);
+
+/** @brief function for validation of inputing ip
+ *  @param ip is ip that will be validate
+ *  @return int
+ */
+int ip_validation(char* ip);
+
+/** @brief ctrl+c catcher */
+void INThandler(int dummy);
+
+/** brief help function */
+void help(void);
+
+/** @brief close all sockets, free allocated memories, and exit the program */
+void all_exit();
+
+int main(int argc, char *argv[])
+{
+	// ctrl+c handler 
+	signal(SIGINT, INThandler);
+    
+    is_connected = 0;
+	struct sockaddr_in servaddr;
+    char *command = (char*)malloc(COMMAND_SIZE * sizeof(char));
+    p_command = command;
+
+    if(argc != 1) {
+    	exit(0);
+    }
+
+	while(1) {
+		command = p_command;
+
+		memset(command, '\0', COMMAND_SIZE);
+        printf("$ ");
+
+        fgets (command, COMMAND_SIZE, stdin);
+        if(strncmp(command, "\n", 1) == 0) {
+            continue; 
+        }
+
+        if(strncmp(command, "exit", 4) == 0) {
+        	all_exit();
+        	exit(0);
+        }
+
+
+        if(strncmp(command, "help", 4) == 0) {
+            help();
+            continue;
+        }
+
+        if(!is_connected) {
+        	if(strncmp(command, "connect", 7) != 0) {
+        		puts("Wrong Command!");
+        		continue;
+        	}
+
+        	char *ip =(char*) malloc(16 * sizeof(char));
+
+        	command += 7;
+			while(*command == ' ') {
+        		command++;
+        	}
+
+            char* pos = strchr(command, ' ');
+            if(pos == NULL) {
+                puts("IP or port doesn't set!");
+                puts("You must write 'connect IP PORT'");
+                continue;
+            }
+        	memcpy(ip, command, pos - command);
+        	char ip_for_validate[sizeof(ip) + 1];
+	
+            
+        	memcpy(ip_for_validate, ip, sizeof(ip) + 1);
+            if(!ip_validation(ip_for_validate)) {
+                puts("Wrong IP!");
+                continue;
+            }   
+
+        	int port;
+   
+			command += strlen(ip);
+			while (*command == ' ') {
+        		command++;
+        	}
+
+			port = atoi(command);
+            if(port < 0 || port > 65535) {
+                puts("Wrong port number!");
+                continue;
+            }
+
+        	// Socket creation and connection to ip and port
+			listening_socket = connection(listening_socket, &servaddr, ip, port);
+
+			is_connected = 1;
+			continue;
+        }
+
+        if((strncmp(command, "get_time", 8) != 0) && (strncmp(command, "disconnect", 10) != 0)) {
+        		puts(command);
+        		puts("Wrong Command!");
+        		continue;
+        }
+
+		// Simple chat for command execution 
+		command_execution(listening_socket, command);
+
+		if ((strncmp(command, "disconnect", 10)) == 0) {
+			command = p_command;
+			is_connected = 0;
+			close (listening_socket);
+    	}
+    }
+	
+	// Close the socket and free alocated spaces
+	free(p_command);
+}
+
+int connection(int listening_socket, struct sockaddr_in* servaddr, char* ip, unsigned short int port)
+{
+	// socket create and verificationi
+	
+	if ((listening_socket = socket(AF_INET , SOCK_STREAM , 0)) < 0)
+	{
+		printf ("Socket creation failed!\n");
+		exit(0);
+	}
+	printf("Socket successfully created!\n ");
+
+	// assign IP and Port
+	servaddr->sin_family = AF_INET;
+	servaddr->sin_addr.s_addr = inet_addr(ip);
+	servaddr->sin_port = htons(port);
+	
+	// Connect the client socket to server socket
+	if(connect(listening_socket, (struct sockaddr*)servaddr, sizeof(*servaddr)) < 0) {
+		printf ("Connection with the server failed!\n");
+		exit(0);
+	}
+	printf("Connected to the server!\n");
+	return listening_socket;
+}
+
+void command_execution(int listening_socket, char* command)
+{
+	int n;
+	int issend, isrecv;
+
+	//Send some data
+	issend = send(listening_socket, command, COMMAND_SIZE, 0);
+	if(issend < 0) {
+		puts("Send failed\n");
+		return ;
+	}
+
+	//Receive a reply from the server
+	memset(command, '\0' , COMMAND_SIZE);
+	isrecv = recv(listening_socket, command, COMMAND_SIZE , 0);
+	if(isrecv < 0) {
+		puts("recv failed\n");
+		return ;
+	}
+
+	printf ("Server reply -> %s\n", command);
+    if ((strncmp(command, "disconnect", 10)) == 0) {
+		printf("Disconncted...\n");
+    }
+}
+
+int ip_validation(char *ip)
+{
+    int num; 
+    int flag = 1; 
+    int counter=0; 
+    char* p = strtok(ip, "."); 
+     
+    while (p && flag ){ 
+        num = atoi(p); 
+     
+        if (num >=0 && num<=255 && (counter++ < 4)) { 
+            flag=1; 
+            p=strtok(NULL,"."); 
+     
+        } 
+        else{ 
+            flag=0; 
+            break; 
+        } 
+    } 
+     
+    return counter == 4;
+}
+
+void all_exit()
+{
+	if (is_connected) {
+    	send(listening_socket, "disconnect", 10, 0);
+	    close (listening_socket);
+	    is_connected = 0;
+    }
+	free(p_command);
+}
+
+void  INThandler(int sig)
+{
+	char  c;
+
+	signal(sig, SIG_IGN);
+	printf("\nOUCH, did you hit Ctrl-C?\n"
+	    "Do you really want to quit? [Y/n] ");
+	c = getchar();
+	if (c == 'y' || c == 'Y' || c == '\n') {
+		all_exit();
+		exit(0);
+	}
+	else {
+		puts("Wrong argument!");
+		printf("$ ");
+		signal(SIGINT, INThandler);
+	}
+	getchar(); // Get new line character
+}
+
+void help()
+{
+    puts("Hey :D I will help You!\n"
+         "connect 'IP' 'PORT'     connecting to mentioned IP's PORT, for example\n"
+         "                        connect 127.0.0.1 8080\n"
+         "get_time                get servers current time\n"
+         "disconnect              disconnect from connected server\n"
+         "exit                    disconnect from connected server and close the program\n"
+         "help                    information about commands");
+}
---
+++ server.c
@@ -0,0 +1,158 @@
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define MESSAGE_LEN 40
+#define PORT 8080
+
+/** @brief socket creation, binding
+ * 	@param listening_socket is socket that will be accepted via server
+ * 	@param client_sock is clients socket
+ * 	@param servaddr for setting ip and port
+ * 	@return void
+ */
+int connection(int listening_socket, int client_sock, struct sockaddr_in* servaddr);
+
+/** @brief comunicating with clients
+ * 	@param client_sock is adress of clients socket 
+ * 	@return void*
+ */
+void* communicating(void* client_sock);
+
+/** @brief viewng, editing or execution of commands grom client
+ * 	@param message is pointer to clients message
+ * 	@return void
+ */
+void command_execution(char* message);
+
+int main ()
+{
+	int listening_socket, client_sock;
+	struct sockaddr_in servaddr;
+
+	// listening_socket creation, binding, assigning with IP and Port
+	client_sock = connection(listening_socket, client_sock, &servaddr);
+
+	// Function for chatting between client and server
+	//communicating(client_sock);
+   
+	// After chatting close the listening_socket
+	close(listening_socket);
+}
+
+int connection(int listening_socket, int client_sock, struct sockaddr_in* servaddr)
+{
+	int* new_sock;
+	int len;
+	struct sockaddr_in* client_adress;
+	// listening_socket create and verification
+	if ((listening_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    	printf("listening_socket creation failed...\n");
+    	exit(0);
+	}
+	printf("listening_socket successfully created..\n");
+	bzero(servaddr, sizeof(*servaddr));
+
+	// assign IP, PORT
+	servaddr->sin_family = AF_INET;
+   	servaddr->sin_addr.s_addr = htonl(INADDR_ANY);
+   	servaddr->sin_port = htons(PORT);
+
+	// Binding newly created listening_socket to given IP and verification
+	const int optional = 1;
+	const socklen_t optional_length = sizeof(optional);
+	setsockopt(listening_socket , SOL_SOCKET, SO_REUSEPORT , &optional , optional_length);
+    if ((bind(listening_socket, (struct sockaddr *)servaddr, sizeof(*servaddr))) < 0) {
+       	printf("listening_socket bind failed...\n");
+       	printf("Something went wrong with read()! %s\n", strerror(errno));
+       	exit(0);
+	}
+        printf("listening_socket successfully binded..\n");
+
+	// Now server is ready to listen and verification
+	if ((listen(listening_socket, 5)) != 0) {
+    	printf("Listen failed...\n");
+   		exit(0);
+	}
+    printf("Server listening..\n");
+   	len = sizeof(client_adress);
+
+	// Accept the data packet from client and verification
+    puts("Waiting for incoming connections...");
+    int result;
+    while(client_sock = accept(listening_socket, (struct sockaddr *)&client_adress, &len)) {
+        pthread_t client_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+
+        if (client_sock < 0) {
+	    	printf("server accept failed...\n");
+		    exit(0);
+    	}
+    printf("server accept the client...\n");
+        result = pthread_create(&client_thread, NULL, communicating ,  (void*) new_sock);
+        if(result < 0) {
+    	    puts("Can't create thread :-(");
+    	    exit(0);
+        }
+    }
+
+ 
+
+	return client_sock;
+}
+
+void* communicating(void* client_sock)
+{
+	int this_sock = *(int*) client_sock;
+	char message[MESSAGE_LEN];
+	int n;
+	int isrecv, issend;
+    isrecv = 0;
+
+	//Receive a message from client
+	while(this_sock > 0)
+	{
+		memset(message, '\0', MESSAGE_LEN);
+		
+		isrecv = recv(this_sock, message, MESSAGE_LEN, 0);
+		if((isrecv < 0) || (strncmp(message, "disconnect", 10) == 0))  {
+			send(this_sock, "disconnect", 10, 0);
+			puts("Client disconnected");
+			break;
+		}
+        
+		printf("From client: %s", message);
+
+		// Work with message
+		command_execution(message);
+
+		//Send the message back to client
+		send(this_sock, message, MESSAGE_LEN, 0);
+		
+	}
+	free(client_sock);
+	return 0;
+}
+
+void command_execution(char* message)
+{
+	if (strncmp(message, "get_time", 8) == 0) {
+	    time_t now;
+	    struct tm *localnow;
+	    time( &now );
+	    localnow = localtime( &now );
+
+	    // Format the struct tm to a new string.
+        char formatted[40];
+	    strftime(formatted, sizeof(formatted), "%Y-%m-%d %T", localnow);
+		strcpy(message, formatted);
+	}
+}
